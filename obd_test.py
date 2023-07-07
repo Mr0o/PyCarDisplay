73d9eeecd5023919414ec91ddbf27876850fa854
Owen Smith <owen.smith1201@gmail.com>
2023-07-07 12:26:46 -0400

    Dummy implementation of the obd module for testing

---
+++ obd_test.py
@@ -0,0 +1,6 @@
+# implementation of the obd module with dummy functions
+# this is used for testing on a non-Raspberry Pi system
+
+class OBD:
+    def __init__(self, address) -> None:
+        pass
