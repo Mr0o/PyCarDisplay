e5513e221c7401b961aabaf895335432710b25f5
Owen Smith <owen.smith1201@gmail.com>
2025-12-13 18:51:41 -0500

    Updated to use $USER

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/pi/Desktop/PyCarDisplay/
-source /home/pi/Desktop/PyCarDisplay/env/bin/activate
+cd /home/$USER/Desktop/PyCarDisplay/
+source /home/$USER/Desktop/PyCarDisplay/env/bin/activate
 python3 main.py
