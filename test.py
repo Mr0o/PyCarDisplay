028e37a9d4369a3ff8d515199a8d4164ab1b6d0d
Owen Smith <owen.smith1201@gmail.com>
2025-12-15 20:13:26 -0500

    Updated test

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import PyCarDisplay.milesLogging as milesLogging
+import PyCarDisplay.miles_logging as miles_logging
 
 # test the milesLogging module
-milesLogging.init_log()
-milesLogging.update_log(100, "1:00")
+miles_logging.init_log()
+miles_logging.update_log(100, "1:00")
 
 # test the rename_log function
-milesLogging.create_new_log()
+miles_logging.create_new_log()
 
