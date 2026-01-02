305b436479da28f919ed6d5f0fd327b74ab69c25
Owen Smith <owen.smith1201@gmail.com>
2026-01-01 21:13:03 -0500

    Added engine temp gauge to display update

--- PyCarDisplay/display.py
+++ PyCarDisplay/display.py
@@ -63,7 +63,7 @@ def LCD_Update(air_temp, engine_temp, mpg_display, gph, time_elapsed, miles_elap
     ### print to LCD ###
     if not USE_METRIC:
         lcdSmall.text("Outside: "+ str(air_temp) +chr(223)+"F", 1)
-        lcdSmall.text("", 2)
+        lcdSmall.text(getTempGauge(engine_temp), 2)
 
         space = ""
         if round(mpg_display) < 10: 
